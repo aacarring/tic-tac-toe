75e8fdd7a4e5c75aeeb0b2fdc50c5e00f65ca839
Alexa Carrington <acarring@wellesley.edu>
2020-11-12 13:16:49 -0500

    Renamed files

--- js/Game.js
+++ js/Game.js
@@ -3,22 +3,27 @@ export default class Game {
         this.turn = "X";
         this.board = new Array(9).fill(null);
     }
+
     nextTurn() {
         this.turn = this.turn === "X" ? "O" : "X";
     }
+
     makeMove(i) {
         if (!this.isInProgress()) {
             return;
         }
+
         if (this.board[i]) {
             return;
         }
-        if (!this.findWinningCombination()) {
-            return;
-        }
+
         this.board[i] = this.turn;
-        this.nextTurn();
+
+        if (!this.findWinningCombination()) {
+            this.nextTurn();
+        }
     }
+
     findWinningCombination() {
         const winningCombinations = [
             [0, 1, 2],
@@ -30,14 +35,18 @@ export default class Game {
             [0, 4, 8],
             [2, 4, 6]
         ];
-        for (let combo of winningCombinations) {
-            const [a, b, c] = combo;
-            if (this.board[a] && (this.board[a] === this.board[b]) && (this.board[a] === this.board[c])) {
-                return combo;
-            } 
+
+        for (const combination of winningCombinations) {
+            const [a, b, c] = combination;
+
+            if (this.board[a] && (this.board[a] === this.board[b] && this.board[a] === this.board[c])) {
+                return combination;
+            }
         }
+
         return null;
     }
+
     isInProgress() {
         return !this.findWinningCombination() && this.board.includes(null);
     }
--- js/script.js
+++
@@ -1,5 +0,0 @@
-import Game from "./game.js";
-import GameView from "./gameView.js";
-
-let game = new Game();
-let gameView = new GameView(document.getElementById('app'));
