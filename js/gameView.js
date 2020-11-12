72b7b91bfb8ae9fe15ac13d4570e54600033cc61
Alexa Carrington <acarring@wellesley.edu>
2020-11-12 12:34:44 -0500

    Added board tile pieces in innerHTML in gameView.js

--- js/gameView.js
+++ js/gameView.js
@@ -13,6 +13,17 @@ export default class GameView {
                     <i class="material-icons">refresh</i>
                 </button>
             </div>
+            <div class="board">
+                <div class="board_tile" data-index="0">0</div>
+                <div class="board_tile" data-index="1">1</div>
+                <div class="board_tile board_tile-winner" data-index="2">2</div>
+                <div class="board_tile" data-index="3">3</div>
+                <div class="board_tile" data-index="4">4</div>
+                <div class="board_tile" data-index="5">5</div>
+                <div class="board_tile" data-index="6">6</div>
+                <div class="board_tile" data-index="7">7</div>
+                <div class="board_tile" data-index="8">8</div>
+            </div>
         `;
     }
 }
