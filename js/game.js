af496446c1dbcee09f4ec5ee8ab1db302a2622fa
Alexa Carrington <acarring@wellesley.edu>
2020-11-12 11:55:20 -0500

    Added functions in game.js and tested logging to console in script.js

--- js/game.js
+++ js/game.js
@@ -3,4 +3,27 @@ export default class Game {
         this.turn = "X";
         this.board = new Array(9).fill(null);
     }
+    nextTurn() {
+        this.turn = this.turn === "X" ? "O" : "X";
+    }
+    makeMove(i) {
+        if (this.board[i]) return;
+        this.board[i] = this.turn;
+        this.nextTurn();
+    }
+    findWinningCombination() {
+        const winningCombinations = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6]
+        ];
+        for (let combo of winningCombinations) {
+            const [a, b, c] = combo;
+        }
+    }
 }
--- js/script.js
+++ js/script.js
@@ -2,5 +2,9 @@ import Game from "./game.js";
 
 let game = new Game();
 
-console.log(game.board);
 console.log(game.turn);
+game.makeMove(0);
+console.log(game.board.join(","));
+console.log(game.turn);
+game.makeMove(6);
+console.log(game.board.join(","));
